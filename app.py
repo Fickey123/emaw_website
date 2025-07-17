72703002d5fe99b3238c32a3cd2b10f3929b9670
Fickey Mwakeh <fickeymwakeh@gmail.com>
2025-07-17 15:27:46 +0300

    Fix: ensure app runs on 0.0.0.0 and Render port

--- app.py
+++ app.py
@@ -1005,5 +1005,5 @@ def parse_other_pastors(raw_text):
 
 # Run the Flask app
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
